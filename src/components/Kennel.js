13a2f09b4e893ec8a9700f3212eff2ba082efccb
Matthew Machurek <mrjm2005@yahoo.com>
2021-04-12 18:56:06 -0500

    added employees, locations and customers

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -2,6 +2,12 @@ import React from "react"
 import "./Kennel.css"
 import { Animal } from "./animal/Animal"
 import "./animal/Animal.css"
+import { Customer } from "./customer/Customer"
+import "./customer/Customer.css"
+import { Employee } from "./employee/Employee"
+import "./employee/Employee.css"
+import { Location } from "./location/Location"
+import "./location/Location.css"
 
 export const Kennel = () => (
     <>
@@ -17,6 +23,27 @@ export const Kennel = () => (
             <Animal />
             <Animal />
             <Animal />
+        </article>
+
+        <h2>Customers</h2>
+        <article className="customers">
+            <Customer />
+            <Customer />
+            <Customer />
+        </article>
+
+        <h2>Employees</h2>
+        <article className="employees">
+            <Employee />
+            <Employee />
+            <Employee />
+        </article>
+
+        <h2>Locations</h2>
+        <article className="locations">
+            <Location />
+            <Location />
+            <Location />
         </article>
     </>
 )
